28296cd5a714760718dde6ac74328c81e232d7ea
Raphael Tarnoczi <raphi.tarnoczi@gmail.com>
2024-09-29 14:13:14 +0200

    added bottom navigation and design for all fragments

--- app/src/main/java/at/tarnoczi/insy_firebase/MainActivity.java
+++ app/src/main/java/at/tarnoczi/insy_firebase/MainActivity.java
@@ -7,18 +7,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import org.checkerframework.checker.units.qual.A;
+
+import at.tarnoczi.insy_firebase.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new AddFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.add) {
+                replaceFragment(new AddFragment());
+            } else if(item.getItemId() == R.id.get) {
+                replaceFragment(new GetFragment());
+            } else if(item.getItemId() == R.id.crash) {
+                replaceFragment(new CrashFragment());
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
