3fba344f0084f23dedaaa6ad0232c3613a3e9054
Raphael Tarnoczi <raphi.tarnoczi@gmail.com>
2024-09-29 14:44:46 +0200

    added crashlytics for my project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
 }
 
 android {
@@ -48,4 +49,5 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-crashlytics")
 }
--- app/src/main/java/at/tarnoczi/insy_firebase/CrashFragment.java
+++ app/src/main/java/at/tarnoczi/insy_firebase/CrashFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.io.IOException;
 
 public class CrashFragment extends Fragment {
     @Override
@@ -14,5 +17,19 @@ public class CrashFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_crash, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button crash = view.findViewById(R.id.crashApp);
+        crash.setOnClickListener(view1 -> {
+            throw new RuntimeException("App crashed!");
+        });
+
+        Button crash2 = view.findViewById(R.id.crashApp2);
+        crash2.setOnClickListener(view1 -> {
+            throw new IllegalArgumentException("App crashed in another way!");
+        });
     }
 }
